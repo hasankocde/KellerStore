98b3bf5e6687b882defccd27c3fa5f1ec0d7b778
haskocde <hasankocoglu21@gmail.com>
2024-08-08 22:59:45 +0200

    homejsx e loading ekledim

--- fr/src/pages/Home.jsx
+++ fr/src/pages/Home.jsx
@@ -1,49 +1,29 @@
-// Home.jsx
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
+import { useQuery } from 'react-query';
 import Banner from "../components/Banner";
 import CategoryIconCard from "../components/cards/CategoryIconCard";
-import AdContainer from "../components/container/AdContainer";
+const AdContainer = React.lazy(() => import("../components/container/AdContainer"));
 import useKellerCall from "../hooks/useKellerCall";
-import { useSelector } from "react-redux";
 import '../index.css';
 
 const Home = () => {
-  const title = "Kaufen und verkaufen Sie alles, von Gebrauchtwagen bis hin zu Mobiltelefonen und Computern, oder suchen Sie weltweit nach Immobilien, Jobs und mehr";
+  const title = "Kaufen und verkaufen Sie alles...";
   const { getLatestAds, getPopularAds, getMostViewedAds } = useKellerCall();
-  const [sortedAds, setSortedAds] = useState({
-    latest: [],
-    popular: [],
-    mostViewed: [],
-  });
 
-  useEffect(() => {
-    const fetchAds = async () => {
-      try {
-        const latest = await getLatestAds(10);
-        const popular = await getPopularAds(10);
-        const mostViewed = await getMostViewedAds(10);
-        setSortedAds({
-          latest,
-          popular,
-          mostViewed,
-        });
-      } catch (error) {
-        console.error('Error fetching ads:', error);
-        // Handle error appropriately, e.g. show error message to user
-      }
-    };
-
-    fetchAds();
-  }, []);
+  const { data: latestAds, isLoading: isLoadingLatest } = useQuery('latestAds', () => getLatestAds(10));
+  const { data: popularAds, isLoading: isLoadingPopular } = useQuery('popularAds', () => getPopularAds(10));
+  const { data: mostViewedAds, isLoading: isLoadingMostViewed } = useQuery('mostViewedAds', () => getMostViewedAds(10));
 
   return (
     <div>
       <Banner title={title} />
       <div className="main-container">
         <CategoryIconCard />
-        <AdContainer title={"Neueste"} ads={sortedAds.latest} />
-        <AdContainer title={"Beliebte"} ads={sortedAds.popular} />
-        <AdContainer title={"Meistgesehene"} ads={sortedAds.mostViewed} />
+        <Suspense fallback={<div>Loading...</div>}>
+          {!isLoadingLatest && <AdContainer title={"Neueste"} ads={latestAds} />}
+          {!isLoadingPopular && <AdContainer title={"Beliebte"} ads={popularAds} />}
+          {!isLoadingMostViewed && <AdContainer title={"Meistgesehene"} ads={mostViewedAds} />}
+        </Suspense>
       </div>
     </div>
   );
